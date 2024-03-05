12e209533950d1240182cd2b421fbf56dc685130
kanhaiya41 <161762630+kanhaiya41@users.noreply.github.com>
2024-03-05 22:03:16 +0530

    new test

---
+++ src/index/java/IndexJava.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package index.java;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author hp
+ */
+public class IndexJava {
+
+    
+    public static void main(String[] args) {
+        JFrame f=new JFrame();
+        f.setSize(1000,700);
+        f.setVisible(true);
+    }
+    
+}
